b33b5a33930168a1e7959687e7b574e28b713cc5
Junior Fernandes <juniorbass8@gmail.com>
2020-07-21 23:39:39 -0300

    Criação da migratiom: Manifestos

---
+++ src/database/migrations/1595385517749-CreateManifests.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateManifests1595385517749 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
