209fcd384ad555384518e212d148e3df17cd0b26
Junior Fernandes <juniorbass8@gmail.com>
2020-07-22 12:13:10 -0300

    Alterando os nomes das tabelas que irão ser criadas atravez das migrations

--- src/database/migrations/1595385476595-CreateOptions.ts
+++ src/database/migrations/1595385476595-CreateOptions.ts
@@ -1,11 +1,89 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateOptions1595385476595 implements MigrationInterface {
-
+export default class CreateUsers1594221116173 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'options_categories',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'cpf',
+                        type: 'int',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'date_of_birth',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'gender',
+                        type: 'int',
+                    },
+                    {
+                        name: 'cell_phone',
+                        type: 'int',
+                    },
+                    {
+                        name: 'address',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'address_neighborhood',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'address_number',
+                        type: 'int',
+                    },
+                    {
+                        name: 'address_complement',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'terms',
+                        type: 'boolean',
+                    },
+                    {
+                        name: 'avatar',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('options_categories');
     }
-
 }
--- src/database/migrations/1595385803385-CreateUsefulLinks.ts
+++ src/database/migrations/1595385803385-CreateUsefulLinks.ts
@@ -1,11 +1,89 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsefulLinks1595385803385 implements MigrationInterface {
-
+export default class CreateUsers1594221116173 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'useful_links',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'cpf',
+                        type: 'int',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'date_of_birth',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'gender',
+                        type: 'int',
+                    },
+                    {
+                        name: 'cell_phone',
+                        type: 'int',
+                    },
+                    {
+                        name: 'address',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'address_neighborhood',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'address_number',
+                        type: 'int',
+                    },
+                    {
+                        name: 'address_complement',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'terms',
+                        type: 'boolean',
+                    },
+                    {
+                        name: 'avatar',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('useful_links');
     }
-
 }
