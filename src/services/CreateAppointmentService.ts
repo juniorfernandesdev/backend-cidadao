acce109c4e8e277abee6203f010cf5bfb86e6317
Junior Fernandes <juniorbass8@gmail.com>
2020-07-22 13:25:39 -0300

    Alterando estrutura

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,33 +5,35 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentRepository';
 
 interface Request {
-  provider_id: string;
-  date: Date;
+    provider_id: string;
+    date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    public async execute({ date, provider_id }: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository,
+        );
 
-    const appointmentDate = startOfHour(date);
+        const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameData = await appointmentsRepository.findByDate(
-      appointmentDate,
-    );
+        const findAppointmentInSameData = await appointmentsRepository.findByDate(
+            appointmentDate,
+        );
 
-    if (findAppointmentInSameData) {
-      throw Error('This appointment is already booked');
+        if (findAppointmentInSameData) {
+            throw Error('This appointment is already booked');
+        }
+
+        const appointment = appointmentsRepository.create({
+            provider_id,
+            date: appointmentDate,
+        });
+
+        await appointmentsRepository.save(appointment);
+
+        return appointment;
     }
-
-    const appointment = appointmentsRepository.create({
-      provider_id,
-      date: appointmentDate,
-    });
-
-    await appointmentsRepository.save(appointment);
-
-    return appointment;
-  }
 }
 
 export default CreateAppointmentService;
--- src/services/CreateUserServices.ts
+++ src/services/CreateUserServices.ts
@@ -4,34 +4,34 @@ import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
-  name: string;
-  email: string;
-  password: string;
+    name: string;
+    email: string;
+    password: string;
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+    public async execute({ name, email, password }: Request): Promise<User> {
+        const usersRepository = getRepository(User);
 
-    const checkUserExists = await usersRepository.findOne({
-      where: { email },
-    });
+        const checkUserExists = await usersRepository.findOne({
+            where: { email },
+        });
 
-    if (checkUserExists) {
-      throw new Error('Email address already user.');
+        if (checkUserExists) {
+            throw new Error('Email address already user.');
+        }
+
+        const hashedPassword = await hash(password, 8);
+
+        const user = usersRepository.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        await usersRepository.save(user);
+        return user;
     }
-
-    const hashedPassword = await hash(password, 8);
-
-    const user = usersRepository.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    await usersRepository.save(user);
-    return user;
-  }
 }
 
 export default CreateUserService;
