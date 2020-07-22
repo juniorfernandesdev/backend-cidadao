6c3f2afef0bcfda120fc6ce6b83a995e745e0ad5
Junior Fernandes <juniorbass8@gmail.com>
2020-07-21 23:38:55 -0300

    Criação da migratiom: Categorias

---
+++ src/database/migrations/1595385442602-CreateCategories.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategories1595385442602 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
