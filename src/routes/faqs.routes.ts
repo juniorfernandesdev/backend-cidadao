44fc8894ff64a690b61d7bb81291533880df2b95
Junior Fernandes <juniorbass8@gmail.com>
2020-07-22 21:34:02 -0300

    Criando a rota POST

--- src/routes/faqs.routes.ts
+++ src/routes/faqs.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import CreateFaqServices from '../services/CreateFaqServices';
 
 const faqsRouter = Router();
 
@@ -7,7 +8,20 @@ faqsRouter.get('/', async (request, response) => {
 });
 
 faqsRouter.post('/', async (request, response) => {
-    return response.json({ message: 'metodo post' });
+    try {
+        const { title, description } = request.body;
+
+        const createFaq = new CreateFaqServices();
+
+        const faq = await createFaq.execute({
+            title,
+            description,
+        });
+
+        return response.json(faq);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
 });
 
 export default faqsRouter;
