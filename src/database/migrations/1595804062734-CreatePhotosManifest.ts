736462a9a643d1a1b0a26914376ede2249ef9e62
Junior Fernandes <juniorbass8@gmail.com>
2020-07-26 19:57:09 -0300

    Criando a migration de Photos Manifests

---
+++ src/database/migrations/1595804062734-CreatePhotosManifest.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreatePhotosManifest1595804062734
+    implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'photos_manifests',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'manifest_id',
+                        type: 'int',
+                    },
+                    {
+                        name: 'photo',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('photos_manifests');
+    }
+}
